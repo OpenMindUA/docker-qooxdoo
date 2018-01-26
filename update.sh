ba1953e185962691fcad99246ab166e38db3a764
Vladislav Plaschevaty <openmind@network.in.ua>
2018-01-26 21:15:37 +0200

    add Dockerfiles

---
+++ update.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+QOOXDOO_VERSIONS=(
+  2.1.2=release_2_1_2
+  3.5.1=release_3_5_1
+  4.1.1=release_4_1_1
+  5.0.2=release_5_0_2
+)
+
+
+for version in "${QOOXDOO_VERSIONS[@]}"; do
+
+version=(${version//=/ })
+qx_version=${version[0]}
+git_tag=${version[1]}
+
+mkdir -p ${git_tag}
+DOCKERFILE=${git_tag}/Dockerfile
+
+/bin/cat <<EOM >${DOCKERFILE}
+FROM python:2.7.14-alpine3.7
+
+RUN apk update \\
+    && apk add git
+
+
+RUN git clone https://github.com/qooxdoo/qooxdoo.git /qooxdoo \\
+    && cd /qooxdoo/ \\
+    && git checkout tags/${git_tag} \\
+    && rm -rf ./.git ./.gitattributes ./.gitignore ./documentation
+
+ENV QOOXDOO_PATH /qooxdoo
+ENV QOOXDOO_VERSION ${qx_version}
+CMD echo qooxdoo version \$QOOXDOO_VERSION
+EOM
+
+done
